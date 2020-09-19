143f6f77785fea59d5bbe7764b5ce8dce49f3b49
Kedar <kedarkulkarni010@gmail.com>
2020-09-20 00:44:18 +0530

    [updated]: SSR scripts

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
   @Input('card') card: any;
 
+    land_success_object;
+    land_success = "NA";
   constructor() { }
 
   ngOnInit(): void {
   }
+
+    ngOnChanges() {
+        this.land_success_object = this.card['rocket']['first_stage']['cores'].length ? this.card['rocket']['first_stage']['cores'][0] : null; 
+        this.land_success = this.land_success_object ? this.land_success_object.land_success : "NA";
+    }
+
   trackByFn(index, item) {
     return item.id;
   }
