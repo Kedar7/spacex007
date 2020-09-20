a477851c8a94b5c2a595cd8696d4c14f69a5c71b
Kedar <kedarkulkarni010@gmail.com>
2020-09-20 15:16:53 +0530

    [updated]: NA error solved

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
   @Input('card') card: any;
+
+  land_success_object;
+  land_success = "NA";
+
+  launch_success_object;
+  launch_success = "NA";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnChanges() {
+    if (this.card['rocket']['first_stage']['cores'][0].land_success == true || this.card['rocket']['first_stage']['cores'][0].land_success == false ) {
+      this.land_success = this.card['rocket']['first_stage']['cores'][0].land_success
+    } else {
+      this.land_success = "NA";
+    }
+
+    if (this.card.launch_success == true || this.card.launch_success == false) {
+      this.launch_success = this.card.launch_success;
+    } else {
+      this.launch_success = "NA";
+    }
+  }
+
   trackByFn(index, item) {
     return item.id;
   }
